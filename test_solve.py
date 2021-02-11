ff118924a25e56ff1ce902223b6da5ef430b3e4f
Caleb Vatral <caleb.m.vatral@vanderbilt.edu>
2021-02-11 12:05:00 -0600

    Reorganize solvers

--- test_solve.py
+++ test_solve.py
@@ -3,5 +3,5 @@ import pyplanning as pp
 #domain, problem = load_pddl("examples/blocksworld.pddl", "examples/stack-blocks.pddl")
 domain, problem = pp.load_pddl("examples/gripper.pddl", "examples/gripper-four.pddl")
 
-plan = pp.search_solve(problem)
+plan = pp.solvers.search_solve(problem)
 print(plan)
