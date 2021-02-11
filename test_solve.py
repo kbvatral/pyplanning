c7478a2e3953b5bae46b182a08c052dbf1498ead
Caleb Vatral <caleb.m.vatral@vanderbilt.edu>
2021-02-11 16:38:37 -0600

    Implement custom heuristic example

--- test_solve.py
+++ test_solve.py
@@ -1,8 +1,27 @@
 import pyplanning as pp
 from pyplanning.solvers.heuristics import ignore_delete_heuristic
+import time
+import math
 
 #domain, problem = load_pddl("examples/blocksworld.pddl", "examples/stack-blocks.pddl")
 domain, problem = pp.load_pddl("examples/gripper.pddl", "examples/gripper-four.pddl")
 
-plan = pp.solvers.search_plan(problem, ignore_delete_heuristic(problem))
-print(plan)
+tic = time.time()
+plan = pp.solvers.search_plan(problem)
+print("Execution Time: %.2f" % (time.time() - tic))
+print("Plan found:")
+print(plan, "\n")
+
+def custom_heuristic(state):
+    num_total = len(problem.goal_state.props)
+    num_complete = 0
+    for prop in state.knowledge:
+        if prop in problem.goal_state.props:
+            num_complete += 1
+    return math.ceil((num_total-num_complete)/2)
+
+tic = time.time()
+plan = pp.solvers.search_plan(problem, custom_heuristic)
+print("Execution Time: %.2f" % (time.time() - tic))
+print("Plan found:")
+print(plan, "\n")
