ccdb6df3e8dfa870df24dc61f5a7c9c3990b92c5
Caleb Vatral <caleb.m.vatral@vanderbilt.edu>
2021-02-23 14:12:06 -0600

    Add Planning Graph class and action mutex to layers

--- pyplanning/solvers/graphplan.py
+++ pyplanning/solvers/graphplan.py
@@ -3,17 +3,13 @@ from ..strips import KnowledgeState, Problem
 from ..action import NopAction
 from ..logic import NOT
 
+
 def graph_plan(problem: Problem):
-    grounded_actions = make_grounded_actions(problem)
-    initial = make_grounded_preds(problem, problem.initial_state.knowledge)
-    state = KnowledgeState(initial, explicit_delete=True)
-    
-    valid_actions = []
-    for ga in grounded_actions:
-        if ga.action.check_preconditions(state, ga.objects):
-            valid_actions.append(ga)
-    level = Level(state, valid_actions)
-    return level
+    graph = PlanningGraph(problem)
+    graph.expand_graph()
+    graph.expand_graph()
+    return graph
+
 
 def make_grounded_actions(problem):
     grounded = []
@@ -23,7 +19,8 @@ def make_grounded_actions(problem):
                 continue
             grounded.append(a.ground(objs))
     return grounded
-    
+
+
 def make_grounded_preds(problem, initial):
     grounded = set(initial)
     for p_name, p in problem.domain.predicates.items():
@@ -35,14 +32,61 @@ def make_grounded_preds(problem, initial):
                 grounded.add(NOT(gp))
     return grounded
 
+
 class Level:
     def __init__(self, state, actions):
         self.prev_state = state
         for prop in self.prev_state.knowledge:
             actions.append(NopAction(prop))
         self.actions = frozenset(actions)
-        
+
         all_effects = set()
         for ga in actions:
             all_effects.update(ga.effects)
-        self.next_state = KnowledgeState(all_effects)
+        self.next_state = KnowledgeState(all_effects, True)
+        
+        self.action_mutex = set()
+        self.get_mutex()
+
+    def get_mutex(self):
+        for actions in itertools.combinations(self.actions, 2):
+            for e in actions[0].effects:
+                # Inconsisent effects
+                if (isinstance(e, NOT) and e.prop in actions[1].effects) or (NOT(e) in actions[1].effects):
+                    self.action_mutex.add(frozenset(actions))
+                # Interference (a1 effects -> a2 precondition)
+                elif (isinstance(e, NOT) and e.prop in actions[1].precondition) or (NOT(e) in actions[1].precondition):
+                    self.action_mutex.add(frozenset(actions))
+            for p in actions[0].precondition:
+                # Interference (a1 precondition -> a2 effects)
+                if (isinstance(p, NOT) and p.prop in actions[1].effects) or (NOT(p) in actions[1].effects):
+                    self.action_mutex.add(frozenset(actions))
+                # Competing needs
+                if (isinstance(p, NOT) and p.prop in actions[1].precondition) or (NOT(p) in actions[1].precondition):
+                    self.action_mutex.add(frozenset(actions))
+
+class PlanningGraph:
+    def __init__(self, problem: Problem):
+        self.problem = problem
+        self.levels = []
+        self.grounded_actions = make_grounded_actions(problem)
+        initial = make_grounded_preds(problem, problem.initial_state.knowledge)
+        self.init_state = KnowledgeState(initial, explicit_delete=True)
+
+    def get_current_state(self):
+        if len(self.levels) == 0:
+            return self.init_state
+        curr_level = self.levels[-1]
+        return curr_level.next_state
+
+    def expand_graph(self):
+        curr_state = self.get_current_state()
+        valid_actions = []
+        for ga in self.grounded_actions:
+            if ga.action.check_preconditions(curr_state, ga.objects):
+                valid_actions.append(ga)
+        level = Level(curr_state, valid_actions)
+        self.levels.append(level)
+
+    def check_goal(self):
+        return self.problem.check_goal(self.get_current_state())
--- test_graph.py
+++ test_graph.py
@@ -2,8 +2,8 @@ import pyplanning as pp
 import itertools
 
 domain, problem = pp.load_pddl("examples/pddl_files/graph/cake.pddl", "examples/pddl_files/graph/haveandeat.pddl")
-level = pp.solvers.graph_plan(problem)
-print(level)
+graph = pp.solvers.graph_plan(problem)
+print(graph.check_goal())
 
 # for a in itertools.product(*[["a1", "a2"]]):
 #     print(a)
