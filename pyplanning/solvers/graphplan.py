0df0c22ba3ee8f08f78e92519b85e11c75074e49
Caleb Vatral <caleb.m.vatral@vanderbilt.edu>
2021-02-25 11:33:32 -0600

    Improve speed of graph expansion through caching NopAction

--- pyplanning/solvers/graphplan.py
+++ pyplanning/solvers/graphplan.py
@@ -37,13 +37,27 @@ def remove_plan_nops(plan):
 
 
 def make_grounded_actions(problem):
+    """
+    Make a set of actions grounded in every possible set of literals
+    and NopActions for every possible predicate
+    """
     grounded = []
     for a_name, a in problem.domain.actions.items():
         for objs in itertools.product(*[problem.get_typed_objs(t) for t in a.types]):
             if len(set(objs)) != len(objs):
                 continue
             grounded.append(a.ground(objs))
-    return grounded
+
+    for p_name, p in problem.domain.predicates.items():
+        for objs in itertools.product(*[problem.get_typed_objs(t) for t in p.types]):
+            if len(set(objs)) != len(objs):
+                continue
+            gp = p.ground(objs)
+            gpn = NOT(gp)
+            grounded.append(NopAction(gp))
+            grounded.append(NopAction(gpn))
+
+    return frozenset(grounded)
 
 
 def make_grounded_preds(problem, initial):
@@ -61,13 +75,9 @@ def make_grounded_preds(problem, initial):
 class Level:
     def __init__(self, prev_layer, actions):
         self.prev_layer = prev_layer
-        for prop in self.prev_layer.state.knowledge:
-            actions.append(NopAction(prop))
         self.actions = frozenset(actions)
 
-        all_effects = set()
-        for ga in actions:
-            all_effects.update(ga.effects)
+        all_effects = get_all_effects(actions)
         self.state = KnowledgeState(all_effects, True)
 
         self.action_mutex = set()
